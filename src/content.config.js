4194d0516be1398ba039d3ebcf46d9490c7be935
Regis Philibert <login@regisphilibert.com>
2025-12-19 14:42:39 -0500

    Add image handling in markdown

--- src/content.config.js
+++ src/content.config.js
@@ -1,7 +1,16 @@
-import { defineCollection } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 const pages = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/" }),
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    slug: z.string().optional(),
+    image: image().optional(),
+    footer_copy: z.string().optional(),
+    seo: z.object({
+      title: z.string().optional()
+    }).optional()
+  })
 });
 
 export const collections = { pages }
--- src/content/contact.md
+++ src/content/contact.md
@@ -4,6 +4,6 @@ seo:
   title: Messages checked daily
 ---
 
-# Send a Letter
+## Send a Letter
 
 [LaurenCerandpr@gmail.com](mailto:laurencerandpr@gmail.com?subject=from%20Lauren%27s%20Website)
